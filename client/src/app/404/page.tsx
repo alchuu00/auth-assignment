97555639f398f99cb5ef8dcf78ef6db670007196
Alja Čekada <alja.cekada@gmail.com>
2024-03-18 21:49:36 -0700

    add error pages

---
+++ client/src/app/404/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Error 404</h1>
+          <p className="py-6">The page could not be found.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error404;
---
+++ client/src/app/500/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Error500 = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Error 500</h1>
+          <p className="py-6">The server encountered an unexpected problem.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error500;
